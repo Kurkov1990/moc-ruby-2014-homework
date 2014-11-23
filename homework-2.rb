5218c824cf2502fa5f7130cbeb1bcf6b51636eba
Kurkov1990 <kurkovandrey90@gmail.com>
2014-11-23 20:18:45 +0200

    Update homework-2.rb

--- homework-2.rb
+++ homework-2.rb
@@ -48,10 +48,46 @@ end
 #Клас Запчастина
 class Detail
   
-   def initialize(appellation, code_detail, price, amount) 
+  def initialize(appellation, code_detail, price, amount) 
       @appellation = appellation
       @code_detail = code_detail
       @price = price
       @amount = amount
   end
   
+  def chek_price(price)
+      if @price = price then 
+        puts "true"
+      else
+        puts "false"
+  
+  def chek_amount(amount)
+      if @amount = amount then 
+        puts "true"
+      else
+        puts "false"
+  end
+  
+  def add_detail
+      puts "Додати запчастину"
+  end
+end
+
+#Клас Послуга
+class Сar
+  
+  def initialize(code_car, number, model, year) 
+      @code_car = code_car
+      @number = number
+      @model = model
+      @year = year
+  end 
+  
+  def write
+      puts "Запис У БД"
+  end
+      
+  def delete
+      puts "Видалення  з БД"
+  end
+end
